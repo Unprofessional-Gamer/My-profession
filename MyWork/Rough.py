2e269ee6f93cc178c1983a5395931305e76a9c97
Unprofessional-Gamer <cs7416865571@gmail.com>
2024-05-28 11:02:57 +0530

    Rough.py

--- MyWork/Rough.py
+++ MyWork/Rough.py
@@ -1,4 +1,5 @@
-from google.cloud import storage
+
+#from google.cloud import storage
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 
@@ -17,17 +18,17 @@ class RemoveUnwantedChars(beam.DoFn):
         custom = [ele.translate(str.maketrans(",#$%^!&-", "         ")) for ele in element]
         return [custom]
 
-def activate_data_cleaning(bucket_name, base_path, dataset, year, file_name):
+def activate_data_cleaning(bucket_name, base_path, file_name):
     options = PipelineOptions(
         project='tnt01-odycda-bld-01-1681',
         runner='DataflowRunner',
-        job_name=f'cautocertmove_{"".join(dataset.split()).lower()}',
+        job_name='cautocertmove',
         temp_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/temp',
         region='europe-west2',
-        staging_location='gs://tnt01-odycda-bid-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
+        staging_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
         service_account_email='svc-dataflow-runner@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
         dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tatei-euwe2-cdp',
-        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odycda-csn-euwe2-kc1-01-bld-01',
+        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odvcda-csn-euwe2-kc1-01-bld-01',
         num_workers=1,
         max_num_workers=3,
         use_public_ips=False,
@@ -37,7 +38,7 @@ def activate_data_cleaning(bucket_name, base_path, dataset, year, file_name):
 
     with beam.Pipeline(options=options) as p:
         file_read = (p
-                     | "Reading the file data" >> beam.io.ReadFromText(f"gs://{bucket_name}/{base_path}/{dataset}/Monthly/{year}/PROCESSED/{file_name}")
+                     | "Reading the file data" >> beam.io.ReadFromText(f"gs://{bucket_name}/{base_path}/RECEIVED/{file_name}")
                      | "Removing duplicates from the file data" >> beam.Distinct()
                      )
 
@@ -50,7 +51,7 @@ def activate_data_cleaning(bucket_name, base_path, dataset, year, file_name):
                                )
 
         (processed_file_read
-         | "Writing to certified zone received folder" >> beam.io.WriteToText(f"gs://tnt01-odycda-bld-01-stb-eu-certzone-386745f0/{base_path}/{dataset}/Monthly/{year}/RECEIVED/{file_name}",
+         | "Writing to certified zone received folder" >> beam.io.WriteToText(f"gs://tnt01-odycda-bld-01-stb-eu-certzone-386745f0/{base_path}/RECEIVED/{file_name}",
                                                                               file_name_suffix="",
                                                                               num_shards=1,
                                                                               shard_name_template="-")
@@ -77,20 +78,20 @@ class VolumeCheck(beam.DoFn):
         yield element
 
 def check_pass_status(element):
-    return all('Passed' in item for item in element[-3:])
+    return all('Passed' in item for item in element[-2:])
 
 def check_fail_status(element):
-    return any('Failed' in item for item in element[-3:])
+    return any('Failed' in item for item in element[-2:])
 
-def start_checks(year, dataset):
+def start_checks():
     options = PipelineOptions(
         project='tnt01-odycda-bld-01-1681',
         runner='DataflowRunner',
-        job_name=f'rawtocertcleaning_{"".join(dataset.split()).lower()}',
+        job_name='rawtocertcleaning_all_files',
         region='europe-west2',
         staging_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
         service_account_email='svc-dataflow-runner@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
-        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tnt01-euwe2-cdp',
+        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tatei-euwe2-cdp',
         subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odycda-csn-euwe2-kc1-01-bld-01',
         num_workers=1,
         max_num_workers=3,
@@ -98,71 +99,70 @@ def start_checks(year, dataset):
         autoscaling_algorithm='THROUGHPUT_BASED',
         save_main_session=True
     )
+
     with beam.Pipeline(options=options) as p:
         bucket_name = 'tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181'
         base_path = 'thParty/MFVS/GFV/update'
-        folder_path = f'{base_path}/{dataset}/Monthly/{year}/RECEIVED/'
+        folder_path = f'{base_path}/Monthly/RECEIVED/'
         client = storage.Client("tnt01-odycda-bld-01-1681")
         blob_list = client.list_blobs(bucket_name, prefix=folder_path)
-        
+
         for blob in blob_list:
             if blob.name.endswith('.csv'):
                 file_name = blob.name.split('/')[-1]
                 inputt = (p
-                          | f"Reading the file data {year}{dataset}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{base_path}/{dataset}/Monthly/{year}/RECEIVED/{file_name}")
+                          | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{blob.name}")
                           | "Split values" >> beam.Map(lambda x: x.split(','))
                           )
 
                 volume_count = (inputt
-                                | f"Calculate volume count {year}{dataset}" >> beam.combiners.Count.Globally()
+                                | f"Calculate volume count {file_name}" >> beam.combiners.Count.Globally()
                                 )
 
                 volume_check = (inputt
-                                | f"Volume check {year}{dataset}" >> beam.ParDo(VolumeCheck(), vol_count=beam.pvalue.AsSingleton(volume_count))
+                                | f"Volume check {file_name}" >> beam.ParDo(VolumeCheck(), vol_count=beam.pvalue.AsSingleton(volume_count))
                                 )
 
-                null_check
                 null_check = (volume_check
-                              | f"Null check {year}{dataset}" >> beam.Filter(lambda x: len(x) > 0)
-                              | f"Null check value {year}{dataset}" >> beam.ParDo(NullCheck())
+                              | f"Null check {file_name}" >> beam.Filter(lambda x: len(x) > 0)
+                              | f"Null check value {file_name}" >> beam.ParDo(NullCheck())
                               )
 
                 passed_records = (null_check
-                                  | f"Filter records with pass status {year}{dataset}" >> beam.Filter(check_pass_status)
-                                  | "Formatting as CSV output format" >> beam.Map(lambda x: ','.join(x[0:-3]))
-                                  | f"Writing to cauzone processed folder {year}{dataset}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/{dataset}/Monthly/{year}/PROCESSED/{file_name}",
-                                                                                                                   file_name_suffix='',
-                                                                                                                   num_shards=1,
-                                                                                                                   shard_name_template='')
+                                  | f"Filter records with pass status {file_name}" >> beam.Filter(check_pass_status)
+                                  | "Formatting as CSV output format" >> beam.Map(lambda x: ','.join(x[:-2]))
+                                  | f"Writing to cauzone processed folder {file_name}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/Monthly/PROCESSED/{file_name}",
+                                                                                                                file_name_suffix='',
+                                                                                                                num_shards=1,
+                                                                                                                shard_name_template='')
                                   )
 
                 failed_records = (null_check
-                                  | f"Filter records with fail status {year}{dataset}" >> beam.Filter(check_fail_status)
+                                  | f"Filter records with fail status {file_name}" >> beam.Filter(check_fail_status)
                                   | "Formatting as CSV output format" >> beam.Map(lambda x: ','.join(x))
-                                  | f"Writing to cauzone error folder {year}{dataset}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/{dataset}/Monthly/{year}/ERROR/{file_name[:4]}_error.csv",
-                                                                                                               file_name_suffix='',
-                                                                                                               num_shards=1,
-                                                                                                               shard_name_template='')
+                                  | f"Writing to cauzone error folder {file_name}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/Monthly/ERROR/{file_name[:4]}_error.csv",
+                                                                                                              file_name_suffix='',
+                                                                                                              num_shards=1,
+                                                                                                              shard_name_template='')
                                   )
 
                 result = p.run()
                 result.wait_until_finish()
 
-                check_status = True
+                check_status = True  # Assuming all checks pass for demonstration purposes
 
                 if check_status:
-                    print("Cert Checks finished")
-                    activate_data_cleaning(bucket_name, base_path, dataset, year, file_name)
+                    print(f"All checks passed for {file_name}")
+                    activate_data_cleaning(bucket_name, base_path, file_name)
                     print("Audit checks completed")
                 else:
-                    print("Cert Checks failed")
+                    print(f"Some checks failed for {file_name}")
 
 def start_data_lister():
-    start_checks(None, None)  # Pass None for dataset and year to process all files in the base path
+    start_checks()
     print("All files in the base path processed")
 
 if __name__ == "__main__":
     print("**..... Starting the data lister ")
     start_data_lister()
     print("**** Data lister completed.. ************")
-
