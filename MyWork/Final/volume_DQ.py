41705469479a051848a5af629cd2d6e59808ac05
Unprofessional-Gamer <cs7416865571@gmail.com>
2024-06-11 09:13:29 +0530

    DQ test

--- MyWork/Final/volume_DQ.py
+++ MyWork/Final/volume_DQ.py
@@ -1,42 +1,134 @@
+import logging
 from google.cloud import storage
-import io
-import csv
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
 
-def count_records(csv_blob):
-    """Count the total number of records in a CSV file."""
-    blob_content = csv_blob.download_as_string()
-    csv_data = io.StringIO(blob_content.decode('utf-8'))
-    csv_reader = csv.reader(csv_data)
-    record_count = sum(1 for row in csv_reader)
-    print(f"File {csv_blob.name} has {record_count} records.")
-    return record_count
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
-def move_file(source_blob, destination_bucket, destination_folder):
-    """Move a file to the specified destination bucket and folder."""
-    destination_blob_name = destination_folder + "/" + source_blob.name.split("/")[-1]
-    destination_bucket.copy_blob(source_blob, destination_bucket, destination_blob_name)
-    source_blob.delete()
-    print(f"File {source_blob.name} moved to {destination_folder}.")
+class CountRecords(beam.DoFn):
+    def process(self, element):
+        yield 1
 
-def main(project_id, source_bucket_name, source_folder_path, destination_bucket_name, destination_folder_path):
-    """Main function to perform data quality checks and move files."""
-    storage_client = storage.Client(project=project_id)
-    source_bucket = storage_client.get_bucket(source_bucket_name)
-    destination_bucket = storage_client.get_bucket(destination_bucket_name)
+def check_pass_status(element):
+    return element > 100
 
-    for blob in source_bucket.list_blobs(prefix=source_folder_path):
-        if blob.name.endswith('.csv'):
-            record_count = count_records(blob)
-            if record_count < 100:
-                move_file(blob, destination_bucket, destination_folder_path + "/Error")
-            else:
-                move_file(blob, destination_bucket, destination_folder_path + "/Processed")
+def log_record_count(file_name, record_count):
+    if record_count:
+        count = record_count[0]
+        logger.info(f"{file_name} has {count} records")
+    else:
+        logger.warning(f"Could not determine the record count for {file_name}")
+
+def activate_data_cleaning(bucket_name, base_path, file_name):
+    options = PipelineOptions(
+        project='tnt01-odycda-bld-01-1681',
+        runner='DataflowRunner',
+        job_name='cautocertmove',
+        region='europe-west2',
+        staging_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
+        service_account_email='svc-dataflow-runner@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
+        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tatei-euwe2-cdp',
+        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odvcda-csn-euwe2-kc1-01-bld-01',
+        num_workers=1,
+        max_num_workers=3,
+        use_public_ips=False,
+        autoscaling_algorithm='THROUGHPUT_BASED',
+        save_main_session=True
+    )
+
+    with beam.Pipeline(options=options) as p:
+        file_read = (p
+                     | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{base_path}/{file_name}")
+                     | f"Removing duplicates from the file data {file_name}" >> beam.Distinct()
+                     )
+
+        processed_file_read = (file_read
+                               | f"Splitting the input data into computational units {file_name}" >> beam.Map(lambda x: x.split(','))
+                               | f"Formatting as CSV output format {file_name}" >> beam.Map(lambda x: ','.join(x))
+                               )
+
+        (processed_file_read
+         | f"Writing to certified zone received folder {file_name}" >> beam.io.WriteToText(f"gs://tnt01-odycda-bld-01-stb-eu-certzone-386745f0/{base_path}/RECEIVED/{file_name}",
+                                                                                          file_name_suffix="",
+                                                                                          num_shards=1,
+                                                                                          shard_name_template="-")
+         )
+
+def start_checks():
+    options = PipelineOptions(
+        project='tnt01-odycda-bld-01-1681',
+        runner='DataflowRunner',
+        job_name='rawtocertcleaning_all_files',
+        region='europe-west2',
+        staging_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
+        service_account_email='svc-dataflow-runner@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
+        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tatei-euwe2-cdp',
+        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odycda-csn-euwe2-kc1-01-bld-01',
+        num_workers=1,
+        max_num_workers=3,
+        use_public_ips=False,
+        autoscaling_algorithm='THROUGHPUT_BASED',
+        save_main_session=True
+    )
+
+    with beam.Pipeline(options=options) as p:
+        bucket_name = 'tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181'
+        base_path = 'thParty/MFVS/GFV/update'
+        folder_path = f'{base_path}/Monthly/RECEIVED/'
+        client = storage.Client("tnt01-odycda-bld-01-1681")
+        blob_list = client.list_blobs(bucket_name, prefix=folder_path)
+
+        for blob in blob_list:
+            if blob.name.endswith('.csv'):
+                file_name = blob.name.split('/')[-1]
+                
+                logger.info(f"Processing file: {file_name}")
+                
+                input = (p
+                          | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{blob.name}")
+                          | "Count records" >> beam.ParDo(CountRecords())
+                          | "Sum record counts" >> beam.CombineGlobally(sum)
+                          )
+
+                output = (input | "Log record count" >> beam.Map(lambda count, file_name=file_name: log_record_count(file_name, [count])))
+
+                passed_records = (input
+                                  | "Filter records with pass status" >> beam.Filter(check_pass_status)
+                                  | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{blob.name}")
+                                  | f"Writing to cauzone processed folder {file_name}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/Monthly/PROCESSED/{file_name}",
+                                                                                                                file_name_suffix='',
+                                                                                                                num_shards=1,
+                                                                                                                shard_name_template='')
+                                  )
+
+                failed_records = (input
+                                  | "Filter records with fail status" >> beam.Filter(lambda x: not check_pass_status(x))
+                                  | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{blob.name}")
+                                  | f"Writing to cauzone error folder {file_name}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/Monthly/ERROR/{file_name[:4]}_error.csv",
+                                                                                                              file_name_suffix='',
+                                                                                                              num_shards=1,
+                                                                                                              shard_name_template='')
+                                  )
+
+                result = p.run()
+                result.wait_until_finish()
+
+                check_status = True  # Assuming all checks passed 
+
+                if check_status:
+                    logger.info(f"All checks passed for {file_name}. Moving to certification stage.")
+                    activate_data_cleaning(bucket_name, base_path, file_name)
+                    logger.info("Audit checks completed")
+                else:
+                    logger.warning(f"Some checks failed for {file_name}")
+
+def start_data_lister():
+    start_checks()
+    logger.info("All files in the base path processed")
 
 if __name__ == "__main__":
-    project_id = "project_id"
-    source_bucket_name = "raw zone bucket"
-    source_folder_path = "3rdParty/MFVS/GFV/testing"
-    destination_bucket_name = "certify zone bucket"
-    destination_folder_path = "3rdParty/MFVS/GFV/Data_Quality"
-
-    main(project_id, source_bucket_name, source_folder_path, destination_bucket_name, destination_folder_path)
+    logger.info("*************************Starting the data lister***************************************")
+    start_data_lister()
+    logger.info("****************************** Data lister completed***********************************")
