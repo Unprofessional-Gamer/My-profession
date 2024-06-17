260239044467e3495e8aab6aba5adafd9ade5cc0
Unprofessional-Gamer <cs7416865571@gmail.com>
2024-06-17 12:57:46 +0530

    DQ test

--- MyWork/Final/final_DQ.py
+++ MyWork/Final/final_DQ.py
@@ -1,163 +1,116 @@
-from google.cloud import storage
 import apache_beam as beam
-from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions
+from google.cloud import storage
+import csv
+from datetime import datetime
+import io
 
-class RemoveDoubleQuotes(beam.DoFn):
-    def process(self, element):
-        custom = [ele.replace("'", "") for ele in element]
-        return [custom]
+class VolumeCheckAndClassify(beam.DoFn):
+    def __init__(self, raw_zone_bucket_name, error_folder_path, processed_folder_path, certify_zone_bucket_name, certify_folder_path):
+        self.raw_zone_bucket_name = raw_zone_bucket_name
+        self.error_folder = error_folder_path
+        self.processed_folder = processed_folder_path
+        self.certify_zone_bucket_name = certify_zone_bucket_name
+        self.certify_folder = certify_folder_path
 
-class FilterNull(beam.DoFn):
-    def process(self, element):
-        null_values = ['null', None, 'Nan', 'NONE', 'Null', 'n', '']
-        if any(ele in null_values for ele in element):
-            element.append('Failed null check')
-        else:
-            element.append('Passed null check')
-        return [element]
-"""class FilterNull(beam.DoFn):
-    def process(self, element):
-        null_values = ['null', None, 'Nan', 'NONE', 'Null', 'n', '']
-        non_empty_columns = [col for col in element if col.strip() not in null_values]
+    def setup(self):
+        self.storage_client = storage.Client()
+
+    def process(self, file_path):
+        print(f"Processing file: {file_path}")
         
-        # If the row has non-empty columns, check each column
-        if len(non_empty_columns) < len(element):  # Means there are empty columns
-            element.append('Failed null check')
+        bucket = self.storage_client.bucket(self.raw_zone_bucket_name)
+        blob = bucket.blob(file_path)
+        content = blob.download_as_string().decode("utf-8")
+        reader = csv.reader(content.splitlines())
+
+        # Count the number of records, ignoring the header row
+        record_count = sum(1 for row in reader) - 1
+        filename = file_path.split("/")[-1]
+
+        if record_count < 100:
+            print(f"File {filename} has less than 100 records, moving to error folder")
+            destination_blob_name = f"{self.error_folder}/{filename}"
+            destination_blob = bucket.blob(destination_blob_name)
+            destination_blob.upload_from_string(content)
         else:
-            element.append('Passed null check')
-            
-        return [element]"""
+            print(f"File {filename} has 100 or more records, moving to processed and certify folder")
+            processed_blob_name = f"{self.processed_folder}/{filename}"
+            certify_blob_name = f"{self.certify_folder}/{filename}"
 
+            # Write to processed folder
+            processed_blob = bucket.blob(processed_blob_name)
+            processed_blob.upload_from_string(content)
 
-class RemoveUnwantedChars(beam.DoFn):
-    def process(self, element):
-        unwanted_chars = ",#$%^!&-"
-        has_unwanted = any(char in ele for ele in element for char in unwanted_chars)
-        if has_unwanted:
-            element.append('Failed char check')
-        else:
-            element.append('Passed char check')
-        return [element]
+            # Write to certify folder in the certify zone bucket
+            certify_bucket = self.storage_client.bucket(self.certify_zone_bucket_name)
+            certify_blob = certify_bucket.blob(certify_blob_name)
+            certify_blob.upload_from_string(content)
+        
+        # Delete the original file
+        blob.delete()
 
-def check_pass_status(element):
-    return all('Passed' in item for item in element[-2:])
+        yield file_path
 
-def check_fail_status(element):
-    return any('Failed' in item for item in element[-2:])
-
-def activate_data_cleaning(bucket_name, base_path, file_name):
+def run_pipeline(project_id, raw_zone_bucket_name, raw_zone_folder_path, classified_folder_path, certify_zone_bucket_name, certify_folder_path):
+    # Configure pipeline options for DataflowRunner
     options = PipelineOptions(
-        project='tnt01-odycda-bld-01-1681',
-        runner='DataflowRunner',
-        job_name='cautocertmove',
+        project=project_id,
+        runner="DataflowRunner",
         region='europe-west2',
-        staging_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
-        service_account_email='svc-dataflow-runner@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
-        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tatei-euwe2-cdp',
-        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odvcda-csn-euwe2-kc1-01-bld-01',
+        staging_location=f'gs://{raw_zone_bucket_name}/staging',
+        service_account_email='svc-dfl-user@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
+        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptoKeys/keyhsm-kms-tnt01-euwe2-cdp',
+        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88h/regions/europe-west2/subnetworks/odycda-csn-euwe2-kc1-01-bld-01',
         num_workers=1,
-        max_num_workers=3,
+        max_num_workers=4,
         use_public_ips=False,
         autoscaling_algorithm='THROUGHPUT_BASED',
         save_main_session=True
     )
 
-    with beam.Pipeline(options=options) as p:
-        file_read = (p
-                     | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{base_path}/{file_name}")
-                     | f"Removing duplicates from the file data {file_name}" >> beam.Distinct()
-                     )
+    print("Initializing Google Cloud Storage client")
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(raw_zone_bucket_name)
+    blobs = [blob.name for blob in bucket.list_blobs(prefix=raw_zone_folder_path) if blob.name.endswith('.csv') and '/' not in blob.name[len(raw_zone_folder_path):].strip('/')]
 
-        processed_file_read = (file_read
-                               | f"Splitting the input data into computational units {file_name}" >> beam.Map(lambda x: x.split(','))
-                               | f"Making the data into standard iterable units {file_name}" >> beam.ParDo(RemoveDoubleQuotes())
-                               | f"Removing the null values in file data {file_name}"  >> beam.ParDo(FilterNull())
-                               | f"Removing the Unwanted characters in file data {file_name}" >> beam.ParDo(RemoveUnwantedChars())
-                               | f"Formatting as CSV output format {file_name}" >> beam.Map(lambda x: ','.join(x))
-                               )
-
-        (processed_file_read
-         | f"Writing to certified zone received folder {file_name}" >> beam.io.WriteToText(f"gs://tnt01-odycda-bld-01-stb-eu-certzone-386745f0/{base_path}/RECEIVED/{file_name}",
-                                                                              file_name_suffix="",
-                                                                              num_shards=1,
-                                                                              shard_name_template="-")
-         )
-
-def start_checks():
-    options = PipelineOptions(
-        project='tnt01-odycda-bld-01-1681',
-        runner='DataflowRunner',
-        job_name='rawtocertcleaning_all_files',
-        region='europe-west2',
-        staging_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
-        service_account_email='svc-dataflow-runner@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
-        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tatei-euwe2-cdp',
-        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odycda-csn-euwe2-kc1-01-bld-01',
-        num_workers=1,
-        max_num_workers=3,
-        use_public_ips=False,
-        autoscaling_algorithm='THROUGHPUT_BASED',
-        save_main_session=True
+    print(f"Found {len(blobs)} CSV files in raw zone folder: {raw_zone_folder_path}")
+    
+    # Define the Beam pipeline
+    p = beam.Pipeline(options=options)
+    
+    files = p | 'Create File List' >> beam.Create(blobs)
+    
+    # Apply the volume check and classification transformation
+    files | 'Volume Check and Classify' >> beam.ParDo(
+        VolumeCheckAndClassify(
+            raw_zone_bucket_name=raw_zone_bucket_name,
+            error_folder_path=f"{classified_folder_path}/error",
+            processed_folder_path=f"{classified_folder_path}/processed",
+            certify_zone_bucket_name=certify_zone_bucket_name,
+            certify_folder_path=certify_folder_path
+        )
     )
 
-    with beam.Pipeline(options=options) as p:
-        bucket_name = 'tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181'
-        base_path = 'thParty/MFVS/GFV/update'
-        folder_path = f'{base_path}/Monthly/RECEIVED/'
-        client = storage.Client("tnt01-odycda-bld-01-1681")
-        blob_list = client.list_blobs(bucket_name, prefix=folder_path)
-
-        for blob in blob_list:
-            if blob.name.endswith('.csv'):
-                file_name = blob.name.split('/')[-1]
-                input = (p
-                          | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{blob.name}")
-                          | "Split values" >> beam.Map(lambda x: x.split(','))
-                          )
-
-                null_check = (input
-                              | f"Null check {file_name}" >> beam.ParDo(FilterNull())
-                              )
-
-                char_check = (null_check
-                              | f"Character check {file_name}" >> beam.ParDo(RemoveUnwantedChars())
-                              )
-
-                passed_records = (char_check
-                                  | f"Filter records with pass status {file_name}" >> beam.Filter(check_pass_status)
-                                  | f"Formatting as CSV output format {file_name}" >> beam.Map(lambda x: ','.join(x[:-2]))
-                                  | f"Writing to cauzone processed folder {file_name}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/Monthly/PROCESSED/{file_name}",
-                                                                                                                file_name_suffix='',
-                                                                                                                num_shards=1,
-                                                                                                                shard_name_template='')
-                                  )
-
-                failed_records = (char_check
-                                  | f"Filter records with fail status {file_name}" >> beam.Filter(check_fail_status)
-                                  | f"Formatting as CSV output format {file_name}" >> beam.Map(lambda x: ','.join(x))
-                                  | f"Writing to cauzone error folder {file_name}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/Monthly/ERROR/{file_name[:4]}_error.csv",
-                                                                                                              file_name_suffix='',
-                                                                                                              num_shards=1,
-                                                                                                              shard_name_template='')
-                                  )
-
-                result = p.run()
-                result.wait_until_finish()
-
-                check_status = True  # Assuming all checks passed 
-
-                if check_status:
-                    print(f"All checks passed for {file_name}")
-                    activate_data_cleaning(bucket_name, base_path, file_name)
-                    print("Audit checks completed")
-                else:
-                    print(f"Some checks failed for {file_name}")
-
-def start_data_lister():
-    start_checks()
-    print("All files in the base path processed")
+    print("Running the Beam pipeline")
+    p.run().wait_until_finish()
+    print("Pipeline execution completed")
 
 if __name__ == "__main__":
-    print("*************************Starting the data lister***************************************")
-    start_data_lister()
-    print("****************************** Data lister completed***********************************")
+    # Define the main function and set the appropriate project and bucket details
+    project_id = 'your-gcp-project-id'
+    raw_zone_bucket_name = 'your-raw-zone-bucket'
+    raw_zone_folder_path = 'your-raw-folder-path'
+    classified_folder_path = 'your-classified-folder-path'  # Placeholder for classified files (processed, error)
+    certify_zone_bucket_name = 'your-certify-zone-bucket'  # Placeholder for processed files in certify zone
+    certify_folder_path = 'your-certify-folder-path'  # Placeholder for processed files in certify zone
+
+    # Run the pipeline
+    run_pipeline(
+        project_id=project_id,
+        raw_zone_bucket_name=raw_zone_bucket_name,
+        raw_zone_folder_path=raw_zone_folder_path,
+        classified_folder_path=classified_folder_path,
+        certify_zone_bucket_name=certify_zone_bucket_name,
+        certify_folder_path=certify_folder_path
+    )
