535ea397a0513637b0d5eace3ee5dfef57501ef3
Unprofessional-Gamer <cs7416865571@gmail.com>
2024-06-23 17:51:46 +0530

    Update schem_file_fetch.py

--- MyWork/Tools/schem_file_fetch.py
+++ MyWork/Tools/schem_file_fetch.py
@@ -1,16 +1,15 @@
 import re
-import pandas as pd
+import csv
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
 from google.cloud import storage
-from io import BytesIO
+from io import BytesIO, StringIO
+import pandas as pd
 
 # Replace with your credentials and bucket information
 bucket_name = 'your-bucket-name'
-raw_folder_path = 'path/to/your/raw/folder'
-excel_output_path = 'path/to/your/output/excel.xlsx'
-
-# Connect to Google Cloud Storage
-client = storage.Client()
-bucket = client.bucket(bucket_name)
+raw_folder_path = 'data/raw/'
+output_csv_path = 'output/consolidated_schemas.csv'
 
 # Regular expression to match folder names like YYYYMMDD-foldername-csv/
 folder_regex = re.compile(r'^\d{8}-([^/]+)-csv/$')
@@ -26,34 +25,64 @@ def list_folders(bucket, prefix):
                 folders.append(match.group(1))
     return folders
 
-# Function to process schema files and write to Excel
-def process_schema_files(bucket, folders, output_path):
-    excel_writer = pd.ExcelWriter(output_path, engine='xlsxwriter')
-    
-    for folder in folders:
-        folder_prefix = f"{folder}-csv/"
-        folder_blobs = bucket.list_blobs(prefix=folder_prefix)
-        
-        for blob in folder_blobs:
-            if blob.name.endswith('.schema.csv'):
-                # Read schema file
-                content = blob.download_as_string()
-                df = pd.read_csv(BytesIO(content))
-                
-                # Prepare data for Excel
-                df['foldername'] = folder
-                df['filename prefix'] = blob.name.split('/')[-1].split('.')[0]
-                
-                # Write to Excel sheet
-                sheet_name = f"{folder}_{blob.name.split('/')[-1].split('.')[0]}"
-                df.to_excel(excel_writer, sheet_name=sheet_name, index=False)
-    
-    excel_writer.save()
+# Function to list schema files in a folder
+def list_schema_files(bucket, folder):
+    folder_prefix = f"{raw_folder_path}{folder}-csv/"
+    schema_files = []
+    blobs = bucket.list_blobs(prefix=folder_prefix)
+    for blob in blobs:
+        if blob.name.endswith('.schema.csv'):
+            schema_files.append(blob.name)
+    return schema_files
 
-# Main script
-if __name__ == "__main__":
-    # List folders matching the pattern
-    folders = list_folders(bucket, raw_folder_path)
+# Function to read schema file and return rows with foldername and filename prefix
+def read_schema_file(file_path):
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(file_path)
+    content = blob.download_as_string()
+    df = pd.read_csv(BytesIO(content))
     
-    # Process schema files and write to Excel
-    process_schema_files(bucket, folders, excel_output_path)
+    foldername = file_path.split('/')[1].split('-')[1]
+    filename_prefix = file_path.split('/')[-1].split('.')[0]
+    
+    df.insert(0, 'filename', filename_prefix)
+    df.insert(0, 'foldername', foldername)
+    
+    output = StringIO()
+    df.to_csv(output, index=False, header=False)
+    return output.getvalue().strip().split('\n')
+
+# Beam Pipeline
+class GCSOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_value_provider_argument('--bucket_name', type=str, help='GCS Bucket Name')
+        parser.add_value_provider_argument('--raw_folder_path', type=str, help='GCS Raw Folder Path')
+        parser.add_value_provider_argument('--output_csv_path', type=str, help='Output CSV Path')
+
+def run():
+    options = PipelineOptions()
+    gcs_options = options.view_as(GCSOptions)
+    
+    with beam.Pipeline(options=options) as p:
+        # Get the list of folders
+        folders = p | 'List Folders' >> beam.Create(list_folders(storage.Client().bucket(bucket_name), raw_folder_path))
+        
+        # Get the list of schema files
+        schema_files = (
+            folders
+            | 'List Schema Files' >> beam.FlatMap(lambda folder: list_schema_files(storage.Client().bucket(bucket_name), folder))
+        )
+        
+        # Read schema files and consolidate data
+        rows = (
+            schema_files
+            | 'Read Schema Files' >> beam.FlatMap(read_schema_file)
+        )
+        
+        # Write to output CSV
+        rows | 'Write to CSV' >> beam.io.WriteToText(output_csv_path, file_name_suffix='.csv', shard_name_template='')
+
+if __name__ == '__main__':
+    run()
