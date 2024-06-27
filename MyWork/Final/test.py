ad08040e74559029175f53c1da8cf83efb4b6606
Unprofessional-Gamer <cs7416865571@gmail.com>
2024-06-27 16:19:49 +0530

    Update test.py

--- MyWork/Final/test.py
+++ MyWork/Final/test.py
@@ -1,163 +1,164 @@
 from google.cloud import storage
-import apache_beam as beam
+import zipfile
+import io
+from datetime import datetime
+import os
 from apache_beam.options.pipeline_options import PipelineOptions
+import apache_beam as beam
+from apache_beam.io import fileio
+from apache_beam.io.fileio import ReadMatches
 
-class RemoveDoubleQuotes(beam.DoFn):
-    def process(self, element):
-        custom = [ele.replace("'", "") for ele in element]
-        return [custom]
+gfv_files = {"CPRRVU", "CPRRVN", "LPRRVN", "LPRRVU"}
 
-class FilterNull(beam.DoFn):
-    def process(self, element):
-        null_values = ['null', None, 'Nan', 'NONE', 'Null', 'n', '']
-        if any(ele in null_values for ele in element):
-            element.append('Failed null check')
-        else:
-            element.append('Passed null check')
-        return [element]
-"""class FilterNull(beam.DoFn):
-    def process(self, element):
-        null_values = ['null', None, 'Nan', 'NONE', 'Null', 'n', '']
-        non_empty_columns = [col for col in element if col.strip() not in null_values]
-        
-        # If the row has non-empty columns, check each column
-        if len(non_empty_columns) < len(element):  # Means there are empty columns
-            element.append('Failed null check')
-        else:
-            element.append('Passed null check')
-            
-        return [element]"""
+def extract_date_from_filename(filename):
+    """Extract date from the filename."""
+    date_str = filename.split('-')[-1].split('.')[0]
+    date_str = date_str[:8]
+    if len(date_str) == 8 and date_str.isdigit():
+        return datetime.strptime(date_str, '%Y%m%d').strftime('%Y-%m-%d')
+    else:
+        return datetime.now().strftime('%Y-%m-%d')
 
+def zip_and_transfer_csv_files(storage_client, raw_zone_bucket, raw_zone_folder_path, consumer_bucket_name, consumer_folder_path, archive_folder_path):
+    """Zip specified files into a ZIP folder."""
+    
+    blob_list = list(raw_zone_bucket.list_blobs(prefix=raw_zone_folder_path))
 
-class RemoveUnwantedChars(beam.DoFn):
-    def process(self, element):
-        unwanted_chars = ",#$%^!&-"
-        has_unwanted = any(char in ele for ele in element for char in unwanted_chars)
-        if has_unwanted:
-            element.append('Failed char check')
-        else:
-            element.append('Passed char check')
-        return [element]
-
-def check_pass_status(element):
-    return all('Passed' in item for item in element[-2:])
-
-def check_fail_status(element):
-    return any('Failed' in item for item in element[-2:])
-
-def activate_data_cleaning(bucket_name, base_path, file_name):
-    options = PipelineOptions(
-        project='tnt01-odycda-bld-01-1681',
-        runner='DataflowRunner',
-        job_name='cautocertmove',
-        region='europe-west2',
-        staging_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
-        service_account_email='svc-dataflow-runner@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
-        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tatei-euwe2-cdp',
-        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odvcda-csn-euwe2-kc1-01-bld-01',
-        num_workers=1,
-        max_num_workers=3,
-        use_public_ips=False,
-        autoscaling_algorithm='THROUGHPUT_BASED',
-        save_main_session=True
-    )
-
-    with beam.Pipeline(options=options) as p:
-        file_read = (p
-                     | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{base_path}/{file_name}")
-                     | f"Removing duplicates from the file data {file_name}" >> beam.Distinct()
-                     )
-
-        processed_file_read = (file_read
-                               | f"Splitting the input data into computational units {file_name}" >> beam.Map(lambda x: x.split(','))
-                               | f"Making the data into standard iterable units {file_name}" >> beam.ParDo(RemoveDoubleQuotes())
-                               | f"Removing the null values in file data {file_name}"  >> beam.ParDo(FilterNull())
-                               | f"Removing the Unwanted characters in file data {file_name}" >> beam.ParDo(RemoveUnwantedChars())
-                               | f"Formatting as CSV output format {file_name}" >> beam.Map(lambda x: ','.join(x))
-                               )
-
-        (processed_file_read
-         | f"Writing to certified zone received folder {file_name}" >> beam.io.WriteToText(f"gs://tnt01-odycda-bld-01-stb-eu-certzone-386745f0/{base_path}/RECEIVED/{file_name}",
-                                                                              file_name_suffix="",
-                                                                              num_shards=1,
-                                                                              shard_name_template="-")
-         )
-
-def start_checks():
-    options = PipelineOptions(
-        project='tnt01-odycda-bld-01-1681',
-        runner='DataflowRunner',
-        job_name='rawtocertcleaning_all_files',
-        region='europe-west2',
-        staging_location='gs://tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181/EXTERNAL/MFVS/PRICING/dataflow/staging',
-        service_account_email='svc-dataflow-runner@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
-        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptokeys/keyhsm-kms-tatei-euwe2-cdp',
-        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88b/regions/europe-west2/subnetworks/odycda-csn-euwe2-kc1-01-bld-01',
-        num_workers=1,
-        max_num_workers=3,
-        use_public_ips=False,
-        autoscaling_algorithm='THROUGHPUT_BASED',
-        save_main_session=True
-    )
-
-    with beam.Pipeline(options=options) as p:
-        bucket_name = 'tnt01-odycda-bld-01-stb-eu-rawzone-52fd7181'
-        base_path = 'thParty/MFVS/GFV/update'
-        folder_path = f'{base_path}/Monthly/RECEIVED/'
-        client = storage.Client("tnt01-odycda-bld-01-1681")
-        blob_list = client.list_blobs(bucket_name, prefix=folder_path)
-
+    if not blob_list:
+        print(f"No files found in {raw_zone_folder_path}.")
+        return
+    
+    try:
+        # create memory zip
+        zip_buffer = {}
         for blob in blob_list:
             if blob.name.endswith('.csv'):
+                date = extract_date_from_filename(blob.name)
+                naming_convention = check_naming_convention(blob.name, date)
                 file_name = blob.name.split('/')[-1]
-                input = (p
-                          | f"Reading the file data {file_name}" >> beam.io.ReadFromText(f"gs://{bucket_name}/{blob.name}")
-                          | "Split values" >> beam.Map(lambda x: x.split(','))
-                          )
+                if naming_convention is not None:
+                    zip_name = os.path.join(consumer_folder_path, naming_convention).replace("\\", "/")
+                    archive_zip_name = os.path.join(archive_folder_path, naming_convention).replace("\\", "/")
+                    if zip_name not in zip_buffer:
+                        zip_buffer[zip_name] = io.BytesIO()
+                    csv_data = blob.download_as_bytes()
+                    with zipfile.ZipFile(zip_buffer[zip_name], 'a', zipfile.ZIP_DEFLATED) as zipf:
+                        zipf.writestr(os.path.basename(blob.name), csv_data)
+                    with zipfile.ZipFile(zip_buffer[archive_zip_name], 'a', zipfile.ZIP_DEFLATED) as zipf:
+                        zipf.writestr(os.path.basename(blob.name), csv_data)
 
-                null_check = (input
-                              | f"Null check {file_name}" >> beam.ParDo(FilterNull())
-                              )
+        for zip_name, zip_buffer in zip_buffer.items():
+            zip_buffer.seek(0)
+            consumer_bucket = storage_client.bucket(consumer_bucket_name)
+            zip_blob = consumer_bucket.blob(zip_name + ".zip")
+            archive_blob = consumer_bucket.blob(archive_zip_name + ".zip")
+            print(f"Zipped blob is: {zip_blob}")
+            zip_blob.upload_from_file(zip_buffer, content_type='application/zip')
+            archive_blob.upload_from_file(zip_buffer, content_type='application/zip')
+        print(f"Uploaded files to {consumer_bucket_name} successfully.")
 
-                char_check = (null_check
-                              | f"Character check {file_name}" >> beam.ParDo(RemoveUnwantedChars())
-                              )
+    except Exception as e:
+        print(f"Error: {e}")
 
-                passed_records = (char_check
-                                  | f"Filter records with pass status {file_name}" >> beam.Filter(check_pass_status)
-                                  | f"Formatting as CSV output format {file_name}" >> beam.Map(lambda x: ','.join(x[:-2]))
-                                  | f"Writing to cauzone processed folder {file_name}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/Monthly/PROCESSED/{file_name}",
-                                                                                                                file_name_suffix='',
-                                                                                                                num_shards=1,
-                                                                                                                shard_name_template='')
-                                  )
+def check_naming_convention(filename, date):
+    date_format = date.replace('-', '')
+    if 'CPRNEW' in filename or 'CDENEW' in filename:
+        return f"{date_format}-CarsNvpo-csv"
+    elif 'CPRVAL' in filename or 'CDEVAL' in filename:
+        return f"{date_format}-BlackBookCodesAndDescriptions-csv"
+    elif 'LDENEW' in filename or 'LPRNEW' in filename:
+        return f"{date_format}-LightsNvpo-csv"
+    elif 'LDEVAL' in filename or 'LPRVAL' in filename:
+        return f"{date_format}-RedLCVBookCodesAndDescriptions-csv"
+    else:
+        return None
 
-                failed_records = (char_check
-                                  | f"Filter records with fail status {file_name}" >> beam.Filter(check_fail_status)
-                                  | f"Formatting as CSV output format {file_name}" >> beam.Map(lambda x: ','.join(x))
-                                  | f"Writing to cauzone error folder {file_name}" >> beam.io.WriteToText(f"gs://{bucket_name}/{base_path}/Monthly/ERROR/{file_name[:4]}_error.csv",
-                                                                                                              file_name_suffix='',
-                                                                                                              num_shards=1,
-                                                                                                              shard_name_template='')
-                                  )
+def copy_and_transfer_csv(raw_zone_bucket, sfg_base_path, consumer_bucket, consumer_folder_path, archive_folder_path, gfv_files):
+    blob_list = list(raw_zone_bucket.list_blobs(prefix=sfg_base_path))
+    for blob in blob_list:
+        file_name = blob.name.split("/")[-1]
+        if any(file_name.startswith(prefix) for prefix in gfv_files):
+            print(f"Processing file: {file_name}")
+            consumer_bucket.blob(f"{consumer_folder_path}/{file_name}").upload_from_string(blob.download_as_bytes(), 'text/csv')
+            consumer_bucket.blob(f"{archive_folder_path}/{file_name}").upload_from_string(blob.download_as_bytes(), 'text/csv')
+            print(f"Moved {file_name} to {consumer_folder_path}/{file_name} and {archive_folder_path}/{file_name}")
+        else:
+            print(f"Ignored file: {file_name}")
 
-                result = p.run()
-                result.wait_until_finish()
+def run_pipeline(project_id, raw_zone_bucket_name, sfg_base_path, consumer_folder_path, archive_folder_path, consumer_bucket_name):
+    options = PipelineOptions(
+        project=project_id,
+        runner="DataflowRunner",
+        region='europe-west2',
+        staging_location=f'gs://{raw_zone_bucket_name}/staging',
+        service_account_email='svc-dfl-user@tnt01-odycda-bld-01-1681.iam.gserviceaccount.com',
+        dataflow_kms_key='projects/tnt01-odykms-bld-01-35d7/locations/europe-west2/keyRings/krs-kms-tnt01-euwe2-cdp/cryptoKeys/keyhsm-kms-tnt01-euwe2-cdp',
+        subnetwork='https://www.googleapis.com/compute/v1/projects/tnt01-hst-bld-e88h/regions/europe-west2/subnetworks/odycda-csn-euwe2-kc1-01-bld-01',
+        num_workers=1,
+        max_num_workers=4,
+        use_public_ips=False,
+        autoscaling_algorithm='THROUGHPUT_BASED',
+        save_main_session=True
+    )
 
-                check_status = True  # Assuming all checks passed 
+    def process_blob(blob_name):
+        storage_client = storage.Client(project=project_id)
+        raw_zone_bucket = storage_client.bucket(raw_zone_bucket_name)
+        consumer_bucket = storage_client.bucket(consumer_bucket_name)
+        
+        blob = raw_zone_bucket.blob(blob_name)
+        date = extract_date_from_filename(blob_name)
+        naming_convention = check_naming_convention(blob_name, date)
+        file_name = blob_name.split('/')[-1]
+        
+        if naming_convention is not None:
+            zip_name = os.path.join(consumer_folder_path, naming_convention).replace("\\", "/")
+            archive_zip_name = os.path.join(archive_folder_path, naming_convention).replace("\\", "/")
+            zip_buffer = io.BytesIO()
+            csv_data = blob.download_as_bytes()
+            with zipfile.ZipFile(zip_buffer, 'a', zipfile.ZIP_DEFLATED) as zipf:
+                zipf.writestr(file_name, csv_data)
+            zip_buffer.seek(0)
+            zip_blob = consumer_bucket.blob(zip_name + ".zip")
+            archive_blob = consumer_bucket.blob(archive_zip_name + ".zip")
+            zip_blob.upload_from_file(zip_buffer, content_type='application/zip')
+            archive_blob.upload_from_file(zip_buffer, content_type='application/zip')
+        
+        return f"Processed {blob_name}"
 
-                if check_status:
-                    print(f"All checks passed for {file_name}")
-                    activate_data_cleaning(bucket_name, base_path, file_name)
-                    print("Audit checks completed")
-                else:
-                    print(f"Some checks failed for {file_name}")
-
-def start_data_lister():
-    start_checks()
-    print("All files in the base path processed")
+    with beam.Pipeline(options=options) as pipeline:
+        files = (
+            pipeline
+            | 'List files' >> fileio.MatchFiles(f'gs://{raw_zone_bucket_name}/{sfg_base_path}/**/*.csv')
+            | 'Read matches' >> ReadMatches()
+            | 'Extract file paths' >> beam.Map(lambda x: x.metadata.path)
+            | beam.Map(print)
+            | 'Process files' >> beam.Map(process_blob)
+        )
 
 if __name__ == "__main__":
-    print("*************************Starting the data lister***************************************")
-    start_data_lister()
-    print("****************************** Data lister completed***********************************")
+    
+    project_id = 'tnt01-odycda-bld-01-1b81'
+    raw_zone_bucket_name = "tnt01-odycda-bld-01-stb-eu-rawzone-d90dce7a"
+    consumer_bucket_name = "tnt01-odycda-bld-01-stb-eu-rawzone-d90dce7a"
+
+    sfg_base_path = "thParty/GFV/Monthly/SFGDrop"
+    consumer_folder_path = 'thParty/GFV/Monthly'
+    
+    # Define the archive folder path with the current date in the format mm-yyyy
+    current_date = datetime.now().strftime('%m-%Y')
+    archive_folder_path = f"thParty/GFV/Monthly/{current_date}/archive"
+
+    storage_client = storage.Client(project=project_id)
+    raw_zone_bucket = storage_client.bucket(raw_zone_bucket_name)
+    consumer_bucket = storage_client.bucket(consumer_bucket_name)
+
+    print("**********CSV file copying started**********")
+    copy_and_transfer_csv(raw_zone_bucket, sfg_base_path, consumer_bucket, consumer_folder_path, archive_folder_path, gfv_files)
+    print("**********CSV file copying completed**********")
+
+    print("**********Files zipping started**********")
+    zip_and_transfer_csv_files(storage_client, raw_zone_bucket, sfg_base_path, consumer_bucket_name, consumer_folder_path, archive_folder_path)
+    print("**********Files zipping completed**********")
+
+    run_pipeline(project_id, raw_zone_bucket_name, sfg_base_path, consumer_folder_path, archive_folder_path, consumer_bucket_name)
